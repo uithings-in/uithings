0cf009828312dff2cd48340ccd7bd78a3d5ca8ee
mriganka-dev <mrigankaghosh.dev@gmail.com>
2026-09-06 01:06:50 +0530

    cleaning up the landing page and removing footer and navbar.

--- frontend-nextjs/app/layout.tsx
+++ frontend-nextjs/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Providers from "./providers";
-import Navbar from "../components/Navbar";
-import { FooterWrapper } from "../components/FooterWrapper";
 
 export const metadata: Metadata = {
   title: "FigComponents",
@@ -25,9 +23,7 @@ export default function RootLayout({
       </head>
       <body>
         <Providers>
-          <Navbar />
           {children}
-          <FooterWrapper />
         </Providers>
       </body>
     </html>
--- frontend-nextjs/app/page.tsx
+++ frontend-nextjs/app/page.tsx
@@ -1,15 +1,7 @@
-import { HeroSection } from "../components/HeroSection";
-import { SectionHowItWorks } from "../components/SectionHowItWorks";
-import { PremiumDesignsSection } from "../components/PremiumDesignsSection";
-import { OperationsSection } from "../components/OperationsSection";
-
 export default function Home() {
   return (
-    <main className="w-full">
-      <HeroSection />
-      <OperationsSection />
-      <SectionHowItWorks />
-      <PremiumDesignsSection />
+    <main className="min-h-screen w-full">
     </main>
   );
 }
+
